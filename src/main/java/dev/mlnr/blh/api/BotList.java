56e6176eea57195a899a9b24be3e67d718f336c3
caneleex <git@mlnr.dev>
2021-02-21 17:56:06 +0100

    add some bot lists

--- src/main/java/dev/mlnr/blh/api/BotList.java
+++ src/main/java/dev/mlnr/blh/api/BotList.java
@@ -1,7 +1,13 @@
 package dev.mlnr.blh.api;
 
 public enum BotList {
-	TOP_GG("https://top.gg/api/bots/%s/stats", "server_count");
+	TOP_GG("https://top.gg/api/bots/%s/stats", "server_count"),
+	BOTLIST_SPACE("https://api.botlist.space/v1/bots/%s", "server_count"),
+	DBOATS("https://discord.boats/api/bot/%s", "server_count"),
+	DSERVICES("https://api.discordservices.net/bot/%s/stats", "servers"),
+	DBOTS_GG("https://discord.bots.gg/api/v1/bots/%s/stats", "guildCount"),
+	DBL("https://discordbotlist.com/api/v1/bots/%s/stats", "guilds"),
+	DEL("https://api.discordextremelist.xyz/v2/bot/%s/stats", "guildCount");
 
 	private final String url;
 	private final String serversParam;
